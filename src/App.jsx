276d167eb9c0021c23102453ce5eceffe82db3df
EITÁN GABRIEL FRIDMAN <48242472@ort>
2025-06-17 09:11:48 -0300

    tp terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [consejo, setConsejo] = useState(null)
+  const [cargando, setCargando] = useState(false);
+
+
+  
+  async function getConsejo() {
+    setCargando(true);
+
+    try {
+      const response = await axios.get('https://api.adviceslip.com/advice');
+      setConsejo(response.data.slip.advice); 
+      setCargando(false);
+    } catch (error) {
+      console.error('Error al obtener el consejo:', error);
+      setConsejo('No se pudo cargar el consejo.');
+      setCargando(false);
+    }
+  }
+  useEffect(() => {
+    getConsejo();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Consejo Aleatorio</h1>
+      {cargando ? (
+        <p>Cargando...</p>
+      ) : (
+        <p>{consejo}</p>
+      )}
+      <button onClick={getConsejo}>Cargar</button>
+    </div>
+  );
+  
 }
-
 export default App
